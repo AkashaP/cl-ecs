cefc643415ac40674b701c9a4c0b6c2eda35d030
Akasha Peppermint <tehrandommagik@gmail.com>
2021-06-27 20:31:00 +1000

    New experimental change on the definition of Systems in ECS

--- system.lisp
+++ system.lisp
@@ -1,11 +1,60 @@
 (in-package :cl-ecs)
 
 (defstruct (system (:conc-name nil))
+  processing
   required
   grouping
   entities)
 
-(defmacro defsys (name (required grouping) &body body)
+;; in most ECS a System owns one family, and uses the family matching patterns "All", "Any" and "None" but i find "All" the only sensible choice, especially since if you use "Any" you might have to distinctify each component in the system logic, defeating the purpose of ECS caching entities for us already
+;; it also tends to treat all entities with similar behaviour, which I don't feel is the purpose of ECS
+
+;; A major difference in this (forked) implementation is that system now owns multiple families
+;; it compares each entity from each family once
+;; This allows distinction between different types of entities
+;; e.g enemy's collision hits player's collision
+;; you have more direct, subtle interaction between the player and the enemy here
+;; you could do things like give only the player invincibility frames but not the enemy
+;; or play a player-only hurt sound
+
+;; if this was normal ECS you would have to do this very indirectly with the data somehow
+;; this still keeps decoupled game logic in my opinion because (now) you can also turn systems on and off
+;; without needing them there
+;; e.g you could iterate over all collision boxes but you dont have a good way of knowing who the collision box belongs to
+
+;; This syntax is a variant of defsys that specifies owned families in a letargs format
+;;
+
+(defmacro defsys-new (name (&body letargs) &rest body)
+  (let ((entities (gensym)))
+    `(progn
+       (setf (gethash ',name (ecs-systems *ecs*))
+             (make-system :processing t
+                          :required ',(mapcar #'cadr letargs)
+                          :grouping ',(mapcar #'car letargs)))
+       (cache-system-entities)
+       (defmethod %do-entities ((system (eql ',name)) &rest ,entities)
+         (block ,name
+           (destructuring-bind ,(mapcar #'car letargs) ,entities
+             ,@body)
+           )))))
+
+(defmacro defsys (name grouping required &body body)
+  "Define a new system."
+  (let ((entities (gensym)))
+    `(progn
+       (setf (gethash ',name (ecs-systems *ecs*))
+             (make-system :required ',required
+                          :grouping ',grouping))
+       (cache-system-entities)
+       (defmethod %do-entities ((system (eql ',name)) &rest ,entities)
+         (block ,name
+           (destructuring-bind ,grouping ,entities
+             ,@body)
+           ))
+       )))
+
+(defmacro defsys-old (name (required grouping) &body body)
   "Define a new system."
   (let ((entities (gensym)))
     `(progn
@@ -22,7 +71,16 @@
 
 (defun all-systems ()
   "Get a list of all defined systems."
-  (hash-table-keys (ecs-systems *ecs*)))
+  (alexandria:hash-table-keys (ecs-systems *ecs*)))
+
+(defun system-processing (system)
+  "Asks whether a system is processing.
+   This only affects (do-system [system]) calls."
+  (processing (gethash system (ecs-systems *ecs*))))
+
+(defun (setf system-processing) (value system)
+  "Turns system processing on or off for (do-system) calls"
+  (setf (processing (gethash system (ecs-systems *ecs*))) value))
 
 (defun required-components (system)
   "Get a list of the specified system's required components."
@@ -38,14 +96,17 @@
 
 (defun collect-system-entities (system)
   "Create a list of all of a system's entities."
-  (loop :with r = (required-components system)
-        :for (id . e) :in (hash-table-alist (ecs-entities *ecs*))
-        :for c = (components e)
-        :when (or (not r)
-                  (and (listp r) (all r c))
-                  (and (eq r :none) (not c))
-                  (and (eq r :any) c))
-          :collect id))
+  (loop :with rs = (required-components system)
+        :for r :in rs
+        collect
+        (loop
+          :for (id . e) :in (alexandria:hash-table-alist (ecs-entities *ecs*))
+          :for c = (components e)
+          :when (or (not r)
+                    (and (listp r) (all r c))
+                    (and (eq r :none) (not c))
+                    (and (eq r :any) c))
+            :collect id)))
 
 (defun system-entities (system)
   "Get a list of all of a system's entities."
@@ -63,15 +124,11 @@
 (defmethod do-system (system)
   "Execute the specified system. The system definition's grouping determines
 parallel processing of entities."
-  (let ((grouping (length (system-grouping system)))
-        (entities (system-entities system))
-        (result))
-    (when (>= (length entities) grouping)
-      (if (= (length entities) 1)
-          (setf result (apply #'%do-entities system entities))
-          (map-combinations
-           (lambda (x) (setf result (apply #'%do-entities system x)))
-           entities :length grouping))
+  (when (system-processing system)
+    (let ((result))
+      (apply #'metatilities:map-combinations
+             (lambda (&rest x) (setf result (apply #'%do-entities system x)))
+             (system-entities system))
       result)))
 
 (defun cycle-systems ()
